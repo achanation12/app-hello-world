dd206e6da69e1d2a16ecdda2649a358b2242ba6d
achanation12 <galihzpcchaikall@gmail.com>
2022-04-20 09:57:05 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/achanation12/app-hello-world
+
+go 1.13
+
+require github.com/achanation12/go-hello-world v1.0.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	hello_world "github.com/achanation12/go-hello-world"
+)
+
+func main() {	
+	fmt.Println(hello_world.HelloWorld())
+}
